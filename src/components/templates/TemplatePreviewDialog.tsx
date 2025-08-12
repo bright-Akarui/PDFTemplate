fe8800665c8bd33565c0a57714ef1a49a0c74c4f
Bright’ Ptp <brice651za@gmail.com>
2025-08-12 10:49:16 +0000

    File changes

--- src/components/templates/TemplatePreviewDialog.tsx
+++ src/components/templates/TemplatePreviewDialog.tsx
@@ -10,7 +10,6 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
-import { Button } from "@/components/ui/button"
 import { Label } from "@/components/ui/label"
 import { Input } from "@/components/ui/input"
 import type { Template } from "@/lib/types"
